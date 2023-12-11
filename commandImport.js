97b42002a3e44af6eb1cdac20f3d1342b93a3c73
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2023-12-11 23:10:56 +0100

    cleanliness

---
+++ commandImport.js
@@ -0,0 +1,24 @@
+import FileUtilities from './modules/FileUtilities/main';
+
+export function installModule(atlas, moduleName) {
+    if (!atlas.loaded)      return ChatLib.chat('&cHPM Atlas hasn\'t loaded yet! Please wait!')
+    if (!moduleName)        return ChatLib.chat('&cPlease input a module name!')
+    if (!atlas[moduleName]) return ChatLib.chat('&cThis module doesn\'t exist!')
+
+    const tempId = Date.now()
+    FileUtilities.urlToFile(atlas[moduleName].url, `./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`, 5000, 5000)
+
+    const suicideCheck = register('step', () => {
+        if (FileUtilities.exists(`./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`)) {
+            suicideCheck.unregister()
+            
+            FileLib.unzip(`./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`, './config/ChatTriggers/modules/')
+
+            FileUtilities.renameDirectory(`./config/ChatTriggers/modules/${atlas[moduleName].expectedName}/`, atlas[moduleName].name)
+            
+            FileLib.delete(`./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`)
+            ChatLib.chat(`&aImported ${atlas[moduleName].name}!`)
+            ChatLib.command('ct load', true)
+        }
+    }).setDelay(1)
+}
--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-import FileUtilities from './modules/FileUtilities/main'
-import axios from './modules/axios'
+import { installModule } from './commandImport';
+import axios from './modules/axios';
 
-let atlas = {
-    loaded: false
-};
+const helpMessage = new Message([
+    '\n',
+    '&7&l■ HPM Commands\n',
+    '&7 | &e/hpm reload &8- &7Reload HPM\'s atlas to check for new mods\n',
+    '&7 | &e/hpm import &8- &7Import a ChatTriggers module from our atlas\n',
+    '&7 | &e&m/hpm install&8 -&7 &mInstall a HPM Package from our Action Library\n',
+    ''
+])
 
-async function init() {
-    atlas = (await axios.get('https://dev.ixnoah.live/mods/hpm/atlas.json')).data
-    atlas.loaded = true
-} init()
+let atlas = { loaded: false };
 
-//command: hpm import
-async function installModule(moduleName) {
-    if (!atlas.loaded) return ChatLib.chat('&cHPM Atlas hasn\'t loaded yet! Please wait!')
-    if (!atlas[moduleName]) return ChatLib.chat('&cThis module doesn\'t exist!')
+// initialisation
+function initialise(loud = false) {
+    axios.get('https://dev.ixnoah.live/mods/hpm/atlas.json')
+    .then((response) => {
+        atlas = response.data
+        atlas.loaded = true
+
+        if (loud) ChatLib.chat('&aHPM has successfully loaded!')
+    })
+        .catch((err) => {
+            ChatLib.chat('&c&lERROR! &eHPM has encountered an error while fetching the atlas! See the console for more info!')
+            return err
+    })
 }
 
-//FileUtilities.urlToFile('','./config/ChatTriggers/modules/', 10000, 10000)
+initialise()
+
+// command initialisation
+register('command', (subcommand, ...args) => {
+    switch(subcommand) {
+        case 'import':
+            installModule(atlas, args[0]?.toLowerCase() || undefined)
+        break;
+        case 'reload':
+            ChatLib.chat('&cReloading HPM...')
+            initialise(true)
+        break;
+        case 'delete':
+            ChatLib.command(`ct delete ${...args}`)
+        break;
+        default:
+            ChatLib.chat(helpMessage)
+    }
+}).setName('hpm')
