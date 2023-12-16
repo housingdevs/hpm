cb3c1fcdcc7283552e6ef3af364a5142e229c0ce
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2023-12-16 23:00:28 +0100

    Importer fixes

--- src/modImporter.js
+++ src/modImporter.js
@@ -1,27 +1,29 @@
-import FileUtilities from './modules/FileUtilities/main';
+import FileUtilities from '../modules/FileUtilities/main';
 
+// im beginnin' to feel like a jank god, jank god.
 export function installModule(atlas, moduleName) {
     if (!atlas.loaded)      return ChatLib.chat('&cHPM Atlas hasn\'t loaded yet! Please wait!')
     if (!moduleName)        return ChatLib.chat('&cPlease input a module name!')
-    if (!atlas[moduleName.split(':')[0]]) return ChatLib.chat('&cThis module doesn\'t exist!') 
+    
+    moduleName = moduleName.split(':')
+    if (!atlas[moduleName[0]]) return ChatLib.chat('&cThis module doesn\'t exist!') 
 
-    let url = atlas[moduleName].releases[moduleName.split(':')[1] || '$main'] // Multi version support
+    let release = atlas[moduleName].releases[moduleName[1] || '$main'] // Multi version support
 
     const tempId = Date.now()
-    FileUtilities.urlToFile(url, `./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`, 5000, 5000)
+    FileUtilities.urlToFile(release.url, `./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`, 5000, 5000)
 
     const suicideCheck = register('step', () => {
         if (FileUtilities.exists(`./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`)) {
             suicideCheck.unregister()
             
+            FileLib.deleteDirectory(`./config/ChatTriggers/modules/${moduleName[0]}`)
             FileLib.unzip(`./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`, './config/ChatTriggers/modules/')
-            // then publish releases u fucker!!!!
-            // lol -master has cause so many problems
-            FileUtilities.renameDirectory(`./config/ChatTriggers/modules/${atlas[moduleName].expectedName}/`, atlas[moduleName].name)
+            FileUtilities.renameDirectory(`./config/ChatTriggers/modules/${release.expectedName}/`, atlas[moduleName[0]].name)
             
             FileLib.delete(`./config/ChatTriggers/modules/hpm-temp-${tempId}.zip`)
-            ChatLib.chat(`&aImported ${atlas[moduleName].name}!`)
-            ChatLib.chat(`&6&lWARN&8 - &7This release has been marked as unstable!`)
+            ChatLib.chat(`&aImported ${atlas[moduleName[0]].name}!`)
+            if (release.unstable) ChatLib.chat(`&6&lWARN&8 - &7This release has been marked as unstable! There may be bugs!`)
             ChatLib.command('ct load', true)
         }
     }).setDelay(1)
